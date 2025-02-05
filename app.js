2e84a9b9849861eb189a43b69b6d7a28e77a45f0
FERSaenzM <fs745542@gmail.com>
2025-02-04 21:40:24 -0600

    Publicación de primer avance de código
    primeros pasos de código del juego

---
+++ app.js
@@ -0,0 +1,32 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
+//Aquí deberás desarrollar la lógica para resolver el problema.
+let nombreCaja = [];
+let cantidadNombres = 0;
+
+function asignarNombres (elemento,texto){
+    let asigna = document.querySelector(elemento);
+    asigna.innerHTML = texto;
+    return;
+}
+
+function insertoAmigo (){
+if (document.getElementById("nombre").value === ""){
+    alert("no has escrito algún nombre");
+}else{
+    nombreCaja.push(document.getElementById("nombre").value);    
+    
+    asignarNombres("h2",`la lista es la siguiente: `); 
+    asignarNombres("h3",nombreCaja);
+    document.getElementById("lista").stylecolor="red";
+    
+    document.getElementById("nombre").value="";
+    console.log(nombreCaja);
+}
+    let cantidadNombres = nombreCaja.length;
+    console.log(cantidadNombres);
+    return;
+}
+
+function generaNumeroRandom (){
+    
+}
